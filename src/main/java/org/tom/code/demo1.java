4eba802a68bb1d9155b3f6771a0cff1a4bb04be4
沈约 <shenyue@517na.com>
2024-10-12 21:44:50 +0800

    day1: 与运算输出int类型的32位二进制

---
+++ src/main/java/org/tom/code/demo1.java
@@ -0,0 +1,27 @@
+package org.tom.code;
+
+/**
+ * 输出int的二进制
+ * 失误点：没有理解到原理，原理是输出32次比如 1000 100 10 1这样的去和需要打印的进行&运算 如果该位上两个都是1，则输出1，对应的与运算若有结果则输出1，若没结果则输出0
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        print(1);
+        print(2);
+        print(3);
+        print(4);
+        print(8);
+    }
+
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print(((num & 1 << i) == 0 ? 0 : 1));
+//            1000
+//            0100
+//            0010
+//            0001
+//            0011
+        }
+        System.out.println();
+    }
+}
